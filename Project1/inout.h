07fc5b5b51cce121d69911debdee6cd79ea27137
mashuull <mari.maslova.06@mail.ru>
2023-05-27 14:45:19 +0500

    first commit

---
+++ Project1/inout.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+void print(const std::vector<int>& vec);
+void read(std::vector<int>& vec);
---
+++ Project1/main.cpp
@@ -0,0 +1,9 @@
+lude <algorithm>
+
+int main() {
+    std::vector<int> vec;
+    read(vec);
+    std::sort(vec.begin(), vec.end());
+    print(vec);
+    return 0;
+}
